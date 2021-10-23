b85b2dd397a604168b0139f9bdc942fe6a3cb922
baroka <birotori@gmail.com>
2021-10-23 10:31:46 +0100

    spinner on submit button

--- app/javascript/spinner.js
+++ app/javascript/spinner.js
@@ -7,3 +7,10 @@ $(document).on('turbolinks:load', function() {
   // hide spinner
   $("#spinner").hide();
 });
+
+$(document).ready(function() {
+   $("#spinner").hide();
+   $('button[type="submit"]').click(function() {
+     $('#spinner').show();
+   });
+});
